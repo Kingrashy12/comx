44947c2940c9bd8c970fe398a9dbc34734542bba
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-06-28 00:50:29 +0100

    chore: update dependencies

--- backend/z_out/zoltra.config.js
+++ backend/z_out/zoltra.config.js
@@ -2,14 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const zoltra_1 = require("zoltra");
 exports.default = (0, zoltra_1.zoltraConfig)({
-  PORT: 8000,
-  LOG_LEVEL: "info",
-  NODE_ENV: "development",
-  error: {
-    displayErrObj: false,
-    showStack: false,
-    includeErrorMessage: true,
-  },
-  plugins: ["./plugins/Error.ts"],
-  disableHandlerError: false,
+    PORT: 8000,
+    LOG_LEVEL: "info",
+    NODE_ENV: "development",
+    error: {
+        displayErrObj: false,
+        showStack: false,
+        includeErrorMessage: true,
+    },
+    plugins: ["./plugins/Error.ts"],
+    disableHandlerError: false,
 });
