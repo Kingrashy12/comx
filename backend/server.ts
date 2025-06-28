5966aab6defb8ed8743f75cdbf0bdaacd2ab613b
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-06-28 02:49:05 +0100

    chore: update zoltra dependency to version 0.1.0-alpha.2.0.15 and refactor server code - Updated zoltra dependency in package.json and package-lock.json to version 0.1.0-alpha.2.0.15. - Commented out import statements and export default in index.ts. - Refactored server.ts to initialize Zoltra and start the server with error handling. - Removed obsolete files and directories related to previous implementation in z_out.

--- backend/server.ts
+++ backend/server.ts
@@ -1,25 +1,25 @@
-import { Zoltra, corsPlugin } from "zoltra";
+import { Logger, Zoltra, corsPlugin } from "zoltra";
 
-// const logger = new Logger("Server");
+const logger = new Logger("Server");
 
-const app = new Zoltra();
-// setup routes, middlewares, etc.
-app.register(corsPlugin());
+// const app = new Zoltra();
+// // setup routes, middlewares, etc.
+// app.register(corsPlugin());
 
-export default app;
+// export default app;
 
-// export async function startServer() {
-//   try {
-//     const app = new Zoltra();
+export async function startServer() {
+  try {
+    const app = new Zoltra();
 
-//     app.register(corsPlugin());
+    app.register(corsPlugin());
 
-//     await app.start();
-//   } catch (error) {
-//     const err = error as Error;
-//     logger.error(`Failed to start server: ${err.message}`);
-//     process.exit(1);
-//   }
-// }
+    await app.start();
+  } catch (error) {
+    const err = error as Error;
+    logger.error(`Failed to start server: ${err.message}`);
+    process.exit(1);
+  }
+}
 
-// startServer();
+startServer();
