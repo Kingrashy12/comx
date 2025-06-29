4b812910ef134137f2139fa0b192defc0fa431a0
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-06-29 03:04:00 +0100

    refactor: enhance request handler and improve route processing

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,9 +1,19 @@
 import { corsPlugin, Zoltra } from "zoltra";
 import routes from "./routes";
+import { IncomingMessage, ServerResponse } from "http";
 
 const app = new Zoltra();
 
 app.register(corsPlugin());
 app.loadStaticRoutes(routes);
 console.log("Routes:", app._routes);
-export default app.handler.bind(app);
+// export default app.handler.bind(app);
+
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+  // strip /api
+  if (req.url?.startsWith("/api")) {
+    req.url = req.url.replace(/^\/api/, "") || "/";
+  }
+  //   await app.start();
+  return app.handler(req, res);
+}
