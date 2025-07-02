fd23391d60bc331706715a4cd13ed0ffdd7d68b8
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-07-02 02:12:16 +0100

    refactor: remove unused API handler and consolidate server configuration

--- backend/server.vercel.ts
+++ backend/server.vercel.ts
@@ -1,4 +1,4 @@
-import { corsPlugin, Zoltra, ZoltraRequest, ZoltraResponse } from "zoltra";
+import { corsPlugin, Zoltra } from "zoltra";
 import { withPrefix } from "zoltra";
 import { routes as authRoutes } from "./routes/auth";
 import { routes as otpRoutes } from "./routes/otp";
@@ -14,7 +14,4 @@ app.register(corsPlugin());
 app.loadStaticRoutes(routes);
 app.loadInit();
 
-export default function handler(req: ZoltraRequest, res: ZoltraResponse) {
-  //   return app.exportHandler()(req, res);
-  res.json({ url: req.url });
-}
+export default app.exportHandler();
