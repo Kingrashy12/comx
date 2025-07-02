0fa145dd32773fa12350a28c2a73d3dd6b6c025f
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-07-02 01:34:08 +0100

    refactor: remove unused handler from [...slug].ts and restore index.ts

--- backend/api/[...slug].ts
+++ backend/api/[...slug].ts
@@ -2,7 +2,6 @@ import { corsPlugin, Zoltra } from "zoltra";
 import { withPrefix } from "zoltra";
 import { routes as authRoutes } from "../routes/auth";
 import { routes as otpRoutes } from "../routes/otp";
-import { IncomingMessage, ServerResponse } from "http";
 
 const routes = [
   ...withPrefix("user", authRoutes),
@@ -15,18 +14,4 @@ app.register(corsPlugin());
 app.loadStaticRoutes(routes);
 app.loadInit();
 
-export default function handler(req: IncomingMessage, res: ServerResponse) {
-  // strip /api
-  // if (req.url?.startsWith("/api")) {
-  //   // Replace /api with an empty string, or default to "/"
-  //   req.url = req.url.replace(/^\/api/, "") || "/";
-  // }
-  // //   await app.start();
-  // return app.handler(req, res);
-
-  res.status(200).json({
-    ok: true,
-    url: req.url,
-    method: req.method,
-  });
-}
+export default app.exportHandler;
---
+++ backend/api/index.ts
@@ -0,0 +1,22 @@
+import { corsPlugin, Zoltra } from "zoltra";
+
+const app = new Zoltra();
+
+app.register(corsPlugin());
+
+app.loadStaticRoutes([
+  {
+    path: "/",
+    method: "GET",
+    handler: (req, res) => {
+      res.json({
+        ok: true,
+        message: "Welcome to Zoltra Serverless Home!",
+      });
+    },
+  },
+]);
+
+app.loadInit();
+
+export default app.exportHandler;
